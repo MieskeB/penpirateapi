88592a9bd4a1cc35e1af63840603b02813ee3388
MieskeB <info@michelbijnen.nl>
2021-03-04 15:13:59 +0100

    feat: started account page

--- user/views.py
+++ user/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse;
 
+
 def index(request):
     return HttpResponse("Hello, world. You're at the user index")
+
+
+def account(request, user_id):
+    return HttpResponse("You're looking at an account with id %s." % user_id)
