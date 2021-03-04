b0cc79ac6453315c1a5ac373e209164e3d4f7503
MieskeB <info@michelbijnen.nl>
2021-03-04 14:52:34 +0100

    feat: added user management to admin

--- user/models.py
+++ user/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.username
