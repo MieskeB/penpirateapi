db84c3fbef79a51a1973c442532076ebb59a070c
MieskeB <info@michelbijnen.nl>
2021-03-04 15:47:26 +0100

    feat: added proper url selection

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'user'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:user_id>/', views.account, name='account')
