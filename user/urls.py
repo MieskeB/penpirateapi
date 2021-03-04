5a745b8d1cc12b7917430a526e8d1cfad10bc9e4
MieskeB <info@michelbijnen.nl>
2021-03-04 16:23:57 +0100

    feat: added register form

--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'user'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:user_id>/', views.account, name='account')
+    path('<int:user_id>/', views.account, name='account'),
+    path('<int:user_id>/updatepassword/', views.update_password, name='update_password')
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+from django.urls import reverse
 
 from .models import User
 
@@ -16,3 +17,24 @@ def index(request):
 def account(request, user_id):
     user = get_object_or_404(User, id=user_id)
     return render(request, 'user/account.html', {'user': user})
+
+
+def update_password(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    try:
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if (password != confirm_password):
+            return render(request, 'user/account.html', {
+                'user': user,
+                'error_message': 'password and confirmed password do not match',
+            })
+    except (KeyError, User.DoesNotExist):
+        return render(request, 'user/account.html', {
+            'user': user,
+            'error_message': 'Something went wrong',
+        })
+    else:
+        user.password = password
+        user.save()
+        return HttpResponseRedirect(reverse('user:account', args=(user.id,)))
