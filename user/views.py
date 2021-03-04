91176f227e759d46997fe5688bda1464c68e041c
MieskeB <info@michelbijnen.nl>
2021-03-04 15:41:17 +0100

    feat: added account pages

--- user/views.py
+++ user/views.py
@@ -1,9 +1,18 @@
-from django.http import HttpResponse;
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+
+from .models import User
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the user index")
+    users_list = User.objects.all()
+    context = {
+        'users_list': users_list,
+    }
+    return render(request, 'user/index.html', context)
 
 
 def account(request, user_id):
-    return HttpResponse("You're looking at an account with id %s." % user_id)
+    user = get_object_or_404(User, id=user_id)
+    return render(request, 'user/account.html', {'user': user})
