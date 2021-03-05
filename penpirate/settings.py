a6729bc8d391d5b14dd008882e97656b452d8a19
Michel Bijnen <35266114+MieskeB@users.noreply.github.com>
2021-03-05 16:52:16 +0100

    Update settings.py

--- penpirate/settings.py
+++ penpirate/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'penpirate.nl'
+]
 
 
 # Application definition
