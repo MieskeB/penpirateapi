2102d37a2bd2caa45713a85037f383e5867d03fb
MieskeB <info@michelbijnen.nl>
2021-05-24 17:17:16 +0200

    feat: added bits

--- tool/urls.py
+++ tool/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'tool'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('generate', views.index, name='generate')
 ]
--- tool/views.py
+++ tool/views.py
@@ -1,3 +1,7 @@
+import mimetypes
+import zipfile
+
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
 from .models import Tool
@@ -12,3 +16,23 @@ def index(request):
         'selectedFile': files[0]
     }
     return render(request, 'tool/index.html', context)
+
+
+def generate(request):
+    code = request.POST['code']
+    cmdname = request.POST['name']
+
+    commandfile = open('command', 'w')
+    commandfile.write('#!/usr/bin/env python3\n\n' + code)
+    makefile = open('makefile', 'w')
+    makefile.write('all: environment rights command\n\nenvironment:\n	sed -i \'1s/^/#!\\/usr\\/bin\\/env python3\\n\\n/\' ./command\n\nrights:\n	chmod +x ./command\n\ncommand:\n	cp ./command /usr/bin/' + cmdname)
+
+    export = zipfile.ZipFile('file.zip', 'w')
+    export.write('command')
+    export.write('makefile')
+
+    download = open('file.zip', 'r')
+    mime_type, _ = mimetypes.guess_type('file.zip')
+    response = HttpResponse(download, content_type=mime_type)
+    response['Content-Disposition'] = "attachment; filename=%s" % 'file.zip'
+    return response
